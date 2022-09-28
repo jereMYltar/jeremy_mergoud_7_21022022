dffb9fdf2e503ec53ce8099af4d4fb02818d4341
jereMYltar <jereMYltar@users.noreply.github.com>
2022-09-28 07:57:25 +0200

    correction du model de la table pivot user_conversation

--- backend/model/user_conversation.model.js
+++ backend/model/user_conversation.model.js
@@ -1,21 +1,47 @@
 const { DataTypes, QueryTypes } = require('sequelize');
 const database = require('../config/database');
+const Conversation = require('./conversation.model');
+const User = require('./user.model');
 
 //définition du modèle correspondant à la table éponyme de la base de donnée
-const UserConversation = database.define('user_conversation', {
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
+const UserConversation = database.define(
+    'user_conversation', 
+    {
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        },
+        conversation_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: Conversation,
+                key: 'id'
+            }
+        },
     },
-    conversation_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-},
-{
-    freezeTableName: true,
-    timestamp: false
-});
+    {
+        freezeTableName: true,
+        timestamps: false
+    }
+);
+
+User.belongsToMany(
+    Conversation,
+    {
+        through: UserConversation,
+        foreignKey: "user_id"
+    });
+Conversation.belongsToMany(
+    User,
+    {
+        through: UserConversation,
+        foreignKey: "conversation_id"
+    });
 
 module.exports = UserConversation;
 
