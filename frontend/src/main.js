f8087edc4754ee16950082d56666bc013f5dfb31
jereMYltar <jereMYltar@users.noreply.github.com>
2022-12-15 21:35:20 +0100

    rendu plus clair de la rédaction

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/style.css";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia).use(router).mount("#app");
