e72cb0df1df2da175fa5a886c38f37b2e7b7984e
jereMYltar <jereMYltar@users.noreply.github.com>
2022-12-15 21:38:49 +0100

    modification de l'ordre des imports ajout des icones ajoutées au projet (profil et gear) affichage du nom de l'utilisateur connecté préparation de l'accès à la consultation / suppression de compte

--- frontend/src/views/ExchangesView.vue
+++ frontend/src/views/ExchangesView.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="main1 container">
+    <div>
+      <h4>Vous êtes connectés en tant que :</h4>
+      <ModalComponent :global="false">
+        <template #callButton>
+          <p>
+            {{ usersStore.activeUser.name }}
+            <img src="../assets/Images/user-solid.svg" height="20" width="20" />
+          </p>
+        </template>
+        <ModalComponent :global="true">
+          <template #callButton>
+            <p>Mon compte</p>
+          </template>
+          <p>Ici le composant de visualisation / modification de compte</p>
+        </ModalComponent>
+        <button @click="logOut">Se déconnecter</button>
+      </ModalComponent>
+    </div>
     <ConversationsList />
     <MessagesList v-if="conversationsStore.activeConversation" />
-    <button @click="logOut">Se déconnecter</button>
   </div>
 </template>
 
 <script setup>
-import EventService from "@/services/EventService.js";
-import ConversationsList from "@/components/conversation/ConversationsList.vue";
-import MessagesList from "@/components/message/MessagesList.vue";
 import { onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useConversationsStore } from "@/store/conversationsStore";
 import { useUsersStore } from "@/store/usersStore";
+import EventService from "@/services/EventService.js";
+import ConversationsList from "@/components/conversation/ConversationsList.vue";
+import MessagesList from "@/components/message/MessagesList.vue";
+import ModalComponent from "@/components/modal/ModalComponent.vue";
 
 const router = useRouter();
 const conversationsStore = useConversationsStore();
@@ -28,9 +46,9 @@ function logOut() {
 
 onMounted(async () => {
   try {
-    if (!usersStore.activeUser) {
+    if (!usersStore.activeUser.id) {
       let response = await EventService.getCurrentUser();
-      usersStore.storeActiveUser(response.data.activeUser);
+      usersStore.addActiveUser(response.data.activeUser);
     }
     let conversations = await EventService.getAllConversationsForCurrentUser();
     conversationsStore.addConversations(conversations.data);
