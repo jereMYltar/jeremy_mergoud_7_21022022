04edb770d0d8237cce63a56aed00846d9c67f254
jereMYltar <jereMYltar@users.noreply.github.com>
2022-12-15 21:36:32 +0100

    transformation du usersStore en format setup ajout d'une ref initiale à activeUser

--- frontend/src/store/usersStore.js
+++ frontend/src/store/usersStore.js
@@ -1,17 +1,21 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useUsersStore = defineStore("users", {
-  state: () => ({
-    users: [],
-    activeUser: null,
-  }),
-  getters: {},
-  actions: {
-    storeActiveUser(payload) {
-      this.activeUser = payload;
-    },
-    addUsers(payload) {
-      this.users = payload;
-    },
-  },
+export const useUsersStore = defineStore("users", () => {
+  //state
+  const users = ref([]);
+  const activeUser = ref({
+    id: 0,
+    name: 0,
+    isAdmin: 0,
+  });
+  //getters
+  //actions
+  function addActiveUser(payload) {
+    this.activeUser = payload;
+  }
+  function addUsers(payload) {
+    this.users = payload;
+  }
+  return { users, activeUser, addActiveUser, addUsers };
 });
