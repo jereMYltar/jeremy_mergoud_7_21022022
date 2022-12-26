a56c012330e8870f103f6959aecd1a142dbf0f65
jereMYltar <jereMYltar@users.noreply.github.com>
2022-12-26 12:28:18 +0100

    nettoyage des commentaires de App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="container">
     <h1 class="titre">Together at Work</h1>
-    <!-- <HomeView></HomeView> -->
     <router-view />
   </div>
 </template>
 
-<script>
-// import HomeView from "@/views/HomeView.vue";
-// export default {
-//   components: { HomeView },
-// };
-</script>
+<script></script>
