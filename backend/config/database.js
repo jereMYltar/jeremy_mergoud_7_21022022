ab0d7bd9dc032f54585a25b0f2a52c74949b0ee7
jereMYltar <jereMYltar@users.noreply.github.com>
2023-01-07 12:39:57 +0100

    commentaire des fichiers de configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,10 @@
-const env = require('../config/env');
-const Sequelize = require('sequelize');
+// configuration de la base de donnée dans sequelize (pour en gérer la connexion)
+const env = require("../config/env");
+const Sequelize = require("sequelize");
 
-const database = new Sequelize(
-    env.DB_NAME,
-    env.DB_USERNAME,
-    env.DB_PASSWORD,
-    {
-        host: env.DB_HOST,
-        dialect: env.DB_DIALECT
-    });
+const database = new Sequelize(env.DB_NAME, env.DB_USERNAME, env.DB_PASSWORD, {
+  host: env.DB_HOST,
+  dialect: env.DB_DIALECT,
+});
 
 module.exports = database;
--- backend/config/env_modele.js
+++ backend/config/env_modele.js
@@ -1,10 +1,15 @@
+// modèle de fichier de configuration contenant les données nécessaires au fonctionnement du serveur :
+// - connexion à la base de donnée MySQL
+// - gestion du mot de passe pour pouvoir devenir administrateur de l'application
+// => le fichier sera à renommer env.js après remplissage
 const env = {
-    DB_NAME: 'nom de la base de données', //remplacer par le nom de la base de donnée mySQL
-    DB_USERNAME: 'nom utilisateur', //remplacer par le nom de l'utilisateur de la BdD
-    DB_PASSWORD: 'mot de passe', //remplacer par le mt de passe de l'utilisateur concerné
-    DB_HOST: 'localhost',
-    DB_DIALECT: 'mysql',
-    ADMIN_PASSWORD: '123', //ou remplacer par un mot de passe administrateur permettant à un utilisateur de devenir lui-même administrateur
+  DB_NAME: "nom de la base de données", //remplacer par le nom de la base de donnée mySQL
+  DB_USERNAME: "nom utilisateur", //remplacer par le nom de l'utilisateur de la BdD
+  DB_PASSWORD: "mot de passe", //remplacer par le mt de passe de l'utilisateur concerné
+  DB_HOST: "localhost",
+  DB_DIALECT: "mysql",
+  JWT_SALT: "clef de salage", //remplacer par une clef de salage pour l'encryptage du mot de passe utilisateur
+  ADMIN_PASSWORD: "123", //remplacer par un mot de passe administrateur permettant à un utilisateur de devenir lui-même administrateur
 };
 
 module.exports = env;
