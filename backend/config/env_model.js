05ba94aa2990101eb4a3b3937e7ad8517202322d
jereMYltar <jereMYltar@users.noreply.github.com>
2023-01-08 22:30:46 +0100

    changement du nom du fichier de configuration modèle dans le backend

--- backend/config/env_model.js
+++ backend/config/env_model.js
@@ -6,7 +6,7 @@ const env = {
   DB_NAME: "nom de la base de données", //remplacer par le nom de la base de donnée mySQL
   DB_USERNAME: "nom utilisateur", //remplacer par le nom de l'utilisateur de la BdD
   DB_PASSWORD: "mot de passe", //remplacer par le mt de passe de l'utilisateur concerné
-  DB_HOST: "localhost",
+  DB_HOST: "localhost", //l'hôte de la base de donnée
   DB_DIALECT: "mysql",
   JWT_SALT: "clef de salage", //remplacer par une clef de salage pour l'encryptage du mot de passe utilisateur
   ADMIN_PASSWORD: "123", //remplacer par un mot de passe administrateur permettant à un utilisateur de devenir lui-même administrateur
