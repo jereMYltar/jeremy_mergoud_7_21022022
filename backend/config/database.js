6e0c9c33c6f0aa13503d99dfec01623be14cb9d2
jereMYltar <jereMYltar@users.noreply.github.com>
2023-01-08 21:52:42 +0100

    ajout d'une vérification de la connexion à la base de donnée MySQL et d'une synchronisation à cette base de donnée

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,33 @@
 const env = require("../config/env");
 const Sequelize = require("sequelize");
 
+// connexion à la base de donnée
 const database = new Sequelize(env.DB_NAME, env.DB_USERNAME, env.DB_PASSWORD, {
   host: env.DB_HOST,
   dialect: env.DB_DIALECT,
 });
+
+// vérification de la connexion à la base de donnée
+(async () => {
+  try {
+    await database.authenticate();
+    console.log("Connexion à la base de donnée réalisée avec succès.");
+  } catch (error) {
+    console.error("Impossibilité de se connecter à la base de donnée :", error);
+  }
+})();
+
+// synchonisation de la base de données
+(async () => {
+  try {
+    await database.sync();
+    console.log("La base de donnée a été synchronisée.");
+  } catch (error) {
+    console.error(
+      "Echec lors de la synchronisation de la base de donnée :",
+      error
+    );
+  }
+})();
 
 module.exports = database;
